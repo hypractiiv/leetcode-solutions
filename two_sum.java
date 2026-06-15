185adc42a6f8ee923a8f1dc13a069115c2f90ed7
Manish R <manishcse18@gmail.com>
2026-06-15 19:58:02 +0530

    Optimize twoSum method for immediate return
    Refactor twoSum method to return solution immediately upon finding a match, removing unnecessary boolean flag.

--- two_sum.java
+++ two_sum.java
@@ -3,18 +3,15 @@ class Solution {
         int[] sol = new int[2];
         for(int i=0;i<nums.length;i++)
         {
-            boolean val=false;
             for(int j=i+1;j<nums.length;j++)
             {
                 if(nums[i]+nums[j]==target)
                 {
                     sol[0]=i;
                     sol[1]=j;
-                    val=true;
-                    break;
+                    return sol;
                 }    
             }
-              if(val) break;
         }
     return sol;
        
